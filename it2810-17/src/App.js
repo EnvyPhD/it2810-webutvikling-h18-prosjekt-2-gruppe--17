c00ad04a52dbba203eec314c8122c3ace1dafa18
Erlend Sæther <erlend.sae@hotmail.com>
2018-09-18 10:45:10 +0200

    added HTML tags for use and added CSS layout for the page so it look ok

--- it2810-17/src/App.js
+++ it2810-17/src/App.js
@@ -14,14 +14,14 @@ class App extends Component {
             <button type="button" name="button" id="tab3">Tab3</button>
             <button type="button" name="button" id="tab4">Tab4</button>
           </div>
-          <div class="img"> <p>image</p>  </div>
-          <div class="txt"> <p>txt</p> </div>
+          <div class="img">  </div>
+          <div class="txt">  </div>
           <div class="categories">
               <div id ="imgcat"></div>
               <div id ="txtcat"></div>
               <div id ="soundcat"></div>
           </div>
-          <div class="sound"> <p>sound</p> </div>
+          <div class="sound"></div>
         </div>
       </body>
     </div>
