c23d518576398fe39db5ea9f745d82187416742f
Erlend Sæther <erlend.sae@hotmail.com>
2018-09-18 11:10:14 +0200

    cleaned up render function

--- it2810-17/src/App.js
+++ it2810-17/src/App.js
@@ -8,20 +8,11 @@ class App extends Component {
       <div className="App">
       <body>
         <div class="wrapper">
-          <div class="buttons">
-            <button type="button" name="button" id="tab1">Tab1</button>
-            <button type="button" name="button" id="tab2">Tab2</button>
-            <button type="button" name="button" id="tab3">Tab3</button>
-            <button type="button" name="button" id="tab4">Tab4</button>
-          </div>
-          <div class="img">  </div>
-          <div class="txt">  </div>
-          <div class="categories">
-              <div id ="imgcat"></div>
-              <div id ="txtcat"></div>
-              <div id ="soundcat"></div>
-          </div>
-          <div class="sound"></div>
+        <Tabs/>
+        <Img/>
+        <Txt/>
+        <Categories/>
+        <Sound/>
         </div>
       </body>
     </div>
@@ -30,3 +21,48 @@ class App extends Component {
 }
 
 export default App;
+
+class Tabs extends Component {
+  render(){
+    return(
+      <div class="buttons">
+        <button type="button" name="button" id="tab1">Tab1</button>
+        <button type="button" name="button" id="tab2">Tab2</button>
+        <button type="button" name="button" id="tab3">Tab3</button>
+        <button type="button" name="button" id="tab4">Tab4</button>
+      </div>
+    )
+  }
+}
+class Img extends Component{
+  render(){
+    return(
+      <div class="img">  </div>
+    )
+  }
+}
+class Txt extends Component{
+  render(){
+    return(
+      <div class="txt">  </div>
+    )
+  }
+}
+class Categories extends Component{
+  render(){
+    return(
+      <div class="categories">
+          <div id ="imgcat"></div>
+          <div id ="txtcat"></div>
+          <div id ="soundcat"></div>
+      </div>
+    )
+  }
+}
+class Sound extends Component{
+  render(){
+    return(
+      <div class="sound"></div>
+    )
+  }
+}
