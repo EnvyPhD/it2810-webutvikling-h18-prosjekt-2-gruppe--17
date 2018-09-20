87a36ead1b0673a1714f6dea81156762cce75482
Per Erik Korssjøen <pek.95@hotmail.com>
2018-09-20 14:37:19 +0200

    worked on adding text

--- it2810-17/src/components/Txt.js
+++ it2810-17/src/components/Txt.js
@@ -3,7 +3,37 @@ import React from "react";
 export default class Txt extends React.Component{
   render(){
     return(
-      <div className="txt">  </div>
+      <div className="txt"> <FetchTxt />  </div>
     )
+  }
+}
+
+class FetchTxt extends React.Component {
+  constructor(props){
+    super(props);
+    var category = "category1";
+    this.state = {
+      data: [null],
+    };
+  }
+
+
+  componentDidMount = (category) => {
+    fetch('http://localhost:3000/text/category1.json')
+      .then(response => response.json())
+      .then(json => {
+        let text = this.state.text
+        // text[category] = json
+        this.setState({text});
+        console.log(text);
+      });
+    };
+
+  render(){
+    return(
+
+      <div className="txtStash"></div>
+    )
+
   }
 }
